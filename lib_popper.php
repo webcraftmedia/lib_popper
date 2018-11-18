35a3448efec28731e7a9727bd89a1b67e994e445
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2018-11-18 22:29:45 +0100

    provide binaries

--- lib_popper.php
+++ lib_popper.php
@@ -4,7 +4,7 @@ class lib_popper extends \LIB\lib_js{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return new \SYSTEM\PLIB('popper/lib/dist/umd/popper.min.js');}
+        return new \SYSTEM\PLIB('popper/dist/umd/popper.min.js');}
     public static function version(){
         return '1.12.9';}
 }
